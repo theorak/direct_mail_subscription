9770b15645b5e966072448e720bd9c8fee080b35
theorak <p.roensch@datec-schmidt.de>
2017-10-20 11:32:21 +0200

    TYPO3 8 Update

--- ext_emconf.php
+++ ext_emconf.php
@@ -35,7 +35,7 @@ $EM_CONF[$_EXTKEY] = array(
     'constraints' => array(
         'depends' => array(
             'tt_address' => '',
-            'typo3' => '6.2.0-7.6.99',
+            'typo3' => '6.2.0-8.7.99',
         ),
         'conflicts' => array(
         ),
